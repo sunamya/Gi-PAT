2f37e9107e649a8a64b748972a0d811d775abe9d
Sunamya Gupta <sunamyagupta@gmail.com>
2021-04-18 17:40:32 +0530

    Removing credentials

--- GPAT/GPAT/connect.py
+++ GPAT/GPAT/connect.py
@@ -1,13 +1,13 @@
 import pyrebase
 
 config={
-	'apiKey': "AIzaSyBfokMPiCPofK6osSCixsOdGsoX3-AhX4g",
-    'authDomain': "gpat-91256.firebaseapp.com",
+	'apiKey': "<YOUT_KEY>",
+    'authDomain': "<YOUR_DOMAIN>",
     'databaseURL': "https://gpat-91256.firebaseio.com",
-    'projectId': "gpat-91256",
-    'storageBucket': "gpat-91256.appspot.com",
-    'messagingSenderId': "1007096476481",
-    'appId': "1:1007096476481:web:b07441e59cd7394e"
+    'projectId': "<ID>",
+    'storageBucket': "<ID>",
+    'messagingSenderId': "<ID>",
+    'appId': "<APP_ID>"
 }
 firebase=pyrebase.initialize_app(config)
 auth=firebase.auth()
